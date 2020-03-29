2e6b4b34153104e9b72c25d45b28fa87ce913821
Allan Farca <allan.farca@gmail.com>
2020-03-29 02:43:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   array.each do |number|
     index_number = array.index(number)
     number_to_square = array [index_number]
-    squared_number = number_to_square**number_to_square
+    squared_number = number_to_square**2
     new_array << squared_number
   end
   array = new_array
